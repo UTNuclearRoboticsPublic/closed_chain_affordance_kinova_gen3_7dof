901e079bd57bee61d47c01950df7af0821f76670
Crasun Jans <jpanthi@utexas.edu>
2024-11-09 14:55:53 -0600

    update the main node to have simpler instructions

--- cca_kinova_gen3_7dof/src/cca_kinova_gen3_7dof_node.cpp
+++ cca_kinova_gen3_7dof/src/cca_kinova_gen3_7dof_node.cpp
@@ -26,6 +26,7 @@ class CcaKinova : public cca_ros::CcaRos
     {
     }
 
+    // Function to run the planner for a given task and/or execute that task on the robot
     bool run(const cc_affordance_planner::PlannerConfig &planner_config,
              const cc_affordance_planner::TaskDescription &task_description,
              const cca_ros::KinematicState &start_config = cca_ros::KinematicState())
@@ -34,6 +35,17 @@ class CcaKinova : public cca_ros::CcaRos
         motion_status_ = std::make_shared<cca_ros::Status>(cca_ros::Status::UNKNOWN);
 
         return this->run_cc_affordance_planner(planner_config, task_description, motion_status_, start_config);
+    }
+
+    // Function overload to plan multiple tasks at once
+    bool run(const std::vector<cc_affordance_planner::PlannerConfig> &planner_configs,
+             const std::vector<cc_affordance_planner::TaskDescription> &task_descriptions,
+             const cca_ros::KinematicState &start_config = cca_ros::KinematicState())
+    {
+        includes_gripper_goal_ = !std::isnan(task_descriptions[0].goal.gripper);
+        motion_status_ = std::make_shared<cca_ros::Status>(cca_ros::Status::UNKNOWN);
+
+        return this->run_cc_affordance_planner(planner_configs, task_descriptions, motion_status_, start_config);
     }
 
     // Function to block until the robot completes the planned trajectory
@@ -78,37 +90,32 @@ int main(int argc, char **argv)
     rclcpp::init(argc, argv);
     rclcpp::NodeOptions node_options;
     node_options.automatically_declare_parameters_from_overrides(true);
-    auto node = std::make_shared<CcaKinova>("cca_ros", node_options, true, false);
+    auto node = std::make_shared<CcaKinova>(
+        "cca_ros", node_options, true,
+        false); ///<-- Flip this false keyword to true to execute the trajectory on the robot
     RCLCPP_INFO(node->get_logger(), "CCA Planner is active");
 
     // Spin the node so joint states can be read
     std::thread spinner_thread([node]() { rclcpp::spin(node); });
 
-    /// REQUIRED INPUT: Task description. See repo README.md Task Examples.
-    // For most cases, all you'll need to do is edit the following block.
+    /// REQUIRED INPUT: Task description. For quick start, the following block provides an example task description to
+    /// do a simple linear motion along the z-axis from the current robot configuration. Edit as needed. See this
+    /// package's demo folder or repo README.md for various other examples that cover motions including rotation, screw,
+    /// cartesian goal, ee orientation jog, etc. It is also possible to plan multiple of these tasks together as a long
+    /// joint trajectory.
     ///------------------------------------------------------------------///
     cc_affordance_planner::TaskDescription task_description;
-    task_description.trajectory_density = 10;
 
+    // Affordance info
     task_description.affordance_info.type = affordance_util::ScrewType::TRANSLATION;
     task_description.affordance_info.axis = Eigen::Vector3d(0, 0, 1);
     task_description.affordance_info.location = Eigen::Vector3d::Zero();
 
-    task_description.goal.affordance = 0.6; // Set desired goal for the affordance
+    // Goals
+    task_description.goal.affordance = 0.5; // Set desired goal for the affordance
 
     cc_affordance_planner::PlannerConfig planner_config;
     ///------------------------------------------------------------------///
-
-    // Optional advanced settings
-    /* task_description.vir_screw_order = affordance_util::VirtualScrewOrder::NONE; */
-    /* planner_config.accuracy = 0.05; // default is 10%, i.e. 0.1 */
-
-    // To plan from a desired start config
-    /* Eigen::VectorXd HOME_CONFIG = */
-    /*     (Eigen::VectorXd(7) << -3.05874e-06, 0.260055, -3.14312, -2.26992, 1.74023e-06,
-     * 0.959945, 1.57006).finished(); */
-    /* cca_ros::KinematicState start_config; */
-    /* start_config.robot = HOME_CONFIG; */
 
     // Run CCA planner and executor
     if (node->run(planner_config, task_description))
